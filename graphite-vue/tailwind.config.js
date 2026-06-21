1d035ab6033809d97b3ef07de1a2efc74b231d45
rauntska <raunokaasik@hotmail.com>
2026-06-21 09:19:13 +0300

    Add custom colors and font family to Tailwind configuration

--- graphite-vue/tailwind.config.js
+++ graphite-vue/tailwind.config.js
@@ -8,7 +8,9 @@ export default {
     extend: {
       colors: {
         slate: {
+          950: '#020617',
           900: '#0f172a',
+          850: '#172033',
           800: '#1e293b',
           700: '#334155',
           600: '#475569',
@@ -16,7 +18,10 @@ export default {
           400: '#94a3b8',
           50: '#f8fafc',
         }
-      }
+      },
+      fontFamily: {
+        tnum: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
     },
   },
   plugins: [],
