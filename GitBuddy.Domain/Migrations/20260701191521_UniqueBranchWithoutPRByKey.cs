772247fa1974c14c3f10bee908fa8316e1f7a30a
rauntska <raunokaasik@hotmail.com>
2026-07-01 22:47:36 +0300

    Add migration to enforce unique index on BranchesWithoutPR by RepoFullName and BranchName

---
+++ GitBuddy.Domain/Migrations/20260701191521_UniqueBranchWithoutPRByKey.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GitBuddy.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueBranchWithoutPRByKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BranchesWithoutPR_Repo_BranchName",
+                table: "BranchesWithoutPR");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BranchesWithoutPR_RepoFullName_BranchName",
+                table: "BranchesWithoutPR",
+                columns: new[] { "RepoFullName", "BranchName" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BranchesWithoutPR_RepoFullName_BranchName",
+                table: "BranchesWithoutPR");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BranchesWithoutPR_Repo_BranchName",
+                table: "BranchesWithoutPR",
+                columns: new[] { "Repo", "BranchName" });
+        }
+    }
+}
