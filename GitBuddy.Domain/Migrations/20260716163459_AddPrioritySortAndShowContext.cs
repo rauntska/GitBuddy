7bda37d769478f3ddccccdbf506de0d15a416174
rauntska <raunokaasik@hotmail.com>
2026-07-20 21:39:20 +0300

    Add PrioritySort and ShowContext preferences to UserPreferences (#12)

---
+++ GitBuddy.Domain/Migrations/20260716163459_AddPrioritySortAndShowContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GitBuddy.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrioritySortAndShowContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PrioritySort",
+                table: "UserPreferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowContext",
+                table: "UserPreferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrioritySort",
+                table: "UserPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "ShowContext",
+                table: "UserPreferences");
+        }
+    }
+}
